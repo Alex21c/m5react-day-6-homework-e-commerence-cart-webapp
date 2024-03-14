5a05cef5f6c49a843ff9915a0454d1bb6e717b2c
Abhishek kumar <aasalex21c@gmail.com>
2024-03-14 18:10:34 +0530

    almost done

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -6,24 +6,97 @@ export default function Cart(){
 
 // fetching products from api
   let reducer = (state, action)=>{
-    if(action.type === 'addProductToCart'){
-      console.log('i have received some data', action.payload.productData, 'state is ', state);
+    // console.log('reducer is called!');
+    if(action.type === 'clearTheCart'){
+      return {
+        products:{}, 
+        cartTotal:0,
+        totalItemsInCart: 0,
+        oneUSDIntINR : 83
+      }
+    }else if(action.type === 'removeProduct'){
+      // console.log('reducer received a reqeust to remove product', action);
+      // console.log('deleting product ', productToBeDeleted);
+
+      // updating cart quantity before deleting
+        let productToBeDeleted = state.products[action.productId];
+        let itemsRemaningInCart = state.totalItemsInCart - productToBeDeleted.quantity;
+        let newCartTotal = state.cartTotal - (productToBeDeleted.quantity * productToBeDeleted.price);
+      // deleting the product
+        let newProducts = {...state.products};
+
+        delete newProducts[action.productId];
+      // updating state        
+       return {
+        ...state,
+        products: newProducts,
+        totalItemsInCart: itemsRemaningInCart,
+        cartTotal : newCartTotal
+      };
+    }    
+    else if(action.type === 'decreaseQuantity'){
+      // console.log('i have received request to decrease quantity',  state.products[action.productId]);
+      // safeguard
+        if(state.products[action.productId].quantity <=1){
+          return state; // no need to decrement
+        }
+      let newProductQuantity = state.products[action.productId].quantity - 1;
+      return {
+        ...state,
+        products: {
+          ...state.products,
+          [action.productId]: {
+            ...state.products[action.productId],
+            quantity: newProductQuantity
+          }
+        },
+        totalItemsInCart: state.totalItemsInCart - 1,
+        cartTotal : state.cartTotal - (1 * state.products[action.productId].price )
+      };
+      
+    }else if(action.type === 'increaseQuantity'){
+      // console.log('i have received request to increase quantity',  state.products[action.productId]);
+      let newProductQuantity = state.products[action.productId].quantity + 1;
+      // console.log(state);
+      return {
+        ...state,
+        products: {
+          ...state.products,
+          [action.productId]: {
+            ...state.products[action.productId],
+            quantity: newProductQuantity
+          }          
+        },
+        totalItemsInCart: state.totalItemsInCart + 1,
+        cartTotal : state.cartTotal + (1 * state.products[action.productId].price)
+      };
+      
+    }else if(action.type === 'addProductToCart'){
+      // console.log('i have received some data', action.payload.productData, 'state is ', state);
+      action.payload.productData.price *=state.oneUSDIntINR;
       return {
         ...state,
         products : {    
           ...state.products,            
           [action.payload.productData.id] : action.payload.productData
+          
         },
-
-        
-        
+        totalItemsInCart: state.totalItemsInCart + 1,
+        cartTotal : state.cartTotal + (action.payload.productData.quantity * action.payload.productData.price)
       }
     }
+    // console.log('reducer is returning default state!')
 
     // default is do nothing
       return state;
   }
-  let [stateCart, dispatch] = new useReducer((reducer), {products:{}, cartTotal:0});
+  let [stateCart, dispatch] = useReducer((reducer), {
+    products:{}, 
+    cartTotal:0,
+    totalItemsInCart: 0,
+    oneUSDIntINR : 83
+
+  });
 // here i will make an api call
   useEffect(()=>{
     try {
@@ -42,7 +115,7 @@ export default function Cart(){
               thumbnail: product.thumbnail
             }
           };
-          console.log(myCustomizedProductData);
+          // console.log(myCustomizedProductData);
           // i will update my cart state
           dispatch({type:'addProductToCart', payload:myCustomizedProductData});
         });
@@ -58,27 +131,42 @@ export default function Cart(){
 
 // Return JSX
   return(
-    <div className="border-2 border-slate-200 p-[2rem] w-[50rem] mt-[2rem] m-auto rounded-md flex flex-col gap-[2rem] text-[1.2rem] text-slate-200">
+    <div id='wrapperEcommerceCartWebApp' className="border-2 border-slate-200 p-[2rem] w-[50rem] mt-[2rem] m-auto rounded-md flex flex-col gap-[2rem] text-[1.2rem] text-slate-200">
       <header className="smallCaps text-center font-semibold text-[2rem]  text-slate-50 flex justify-between ">
         <h1>e-Commerence Cart WebApp using useReducer</h1>
         <div className="flex relative  bg-blue-500 text-[2.5rem]">
           <i className="fa-sharp fa-solid fa-cart-plus absolute right-[1rem] "></i>
-          <span className="text-[1.3rem] text-slate-900 absolute bg-yellow-300 rounded-full pt-[0.1rem] pb-[0.1rem] p-[.7rem] top-[-0.4rem] right-[-.2rem]">4</span>
+          <span className="text-[1.3rem] text-slate-900 absolute bg-yellow-300 rounded-full pt-[0.1rem] pb-[0.1rem] p-[.7rem] top-[-0.4rem] right-[-.2rem]">{stateCart.totalItemsInCart}</span>
         </div>
       </header>
       <section className="flex flex-col gap-[2rem]">
         <h2 className="smallCaps text-slate-50 text-[2rem] text-center">Yours Shopping Cart</h2>
-        <div className="flex flex-col gap-[2rem]">
+        <div>
           {
-          Object.entries(stateCart.products).map(([idx, product])=><CartItem key={idx} product={product}/>)
+            (stateCart.totalItemsInCart === 0) ? <div className="text-[2rem] text-center text-yellow-200">Yours Shopping Cart is Empty!</div> : ""
+
+            
+          }
+        </div>
+        <div className="wrapperAllProducts flex flex-col gap-[2rem]">
+          {
+          Object.entries(stateCart.products).map(([idx, product])=><CartItem key={idx} product={product} dispatch={dispatch}/>)
 
           }
         </div>
-        <div className="border-t-2 border-yellow-100 p-[1rem] flex justify-between">
+        <div className="wrapperTotal border-t-2 border-yellow-100 p-[1rem] flex justify-between">
           <h2 className="smallCaps text-slate-50 text-[2rem] font-semibold">Total</h2>
           <div className="font-semibold  outline outline-2 outline-amber-50 bg-yellow-300 hover:bg-yellow-500 transition cursor-pointer px-[2rem] p-[.5rem] rounded-md hover:text-slate-50 text-slate-900 text-[1.5rem]">
-            ₹2550
+            ₹{stateCart.cartTotal}
           </div>
+        </div>
+        <div>
+          <button className="outline-amber-50 bg-yellow-300 hover:bg-yellow-500 transition cursor-pointer px-[2rem] p-[.5rem] rounded-md hover:text-slate-50 text-slate-900 text-[1.5rem]" onClick={
+            
+            ()=>{
+              dispatch({type:'clearTheCart'})
+            }            
+          }>Clear the Cart</button>
         </div>
 
       </section>
--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -1,37 +1,42 @@
-export default function CartItem({product}){
+export default function CartItem({product, dispatch}){
 
-  console.log('*** inside cartItem, ' , product);
+  // console.log('Inside cartItem, ' , product);
 
 
  
 
-// setting base conversion
-  let oneUSDIntINR = 83;
 
 // here goes our JSX
   return (
-    <div className="flex gap-[1rem] justify-between text-[1.5rem]">
-      <div className="flex gap-[1rem]">
-        <div className="w-[15rem] h-[15rem]">
+    <div className="wrapperProduct flex gap-[1rem] justify-between text-[1.5rem]">
+      <div className="wrapperProductImageAndTitle flex gap-[1rem]">
+        <div className="productImage w-[15rem] h-[15rem]">
           <img className="object-fill w-[100%] h-[100%] rounded-md shadow-md shadow-yellow-300" src={product.thumbnail} alt={`Image-${product.title}`} />
         </div>
-        <div className="flex flex-col gap-[.5rem]">
-          <span className="capitalize font-semibold">{product.title}</span>
-          <span>₹ {product.price * oneUSDIntINR * product.quantity}</span>
-          <span className="font-semibold  outline outline-2 outline-amber-50 bg-yellow-300 hover:bg-yellow-500 transition cursor-pointer px-[1rem] p-[.3rem] rounded-md hover:text-slate-50 text-slate-900 text-[1rem] max-w-[6rem]">Remove</span>
+        <div className="titleAndRemoveBtn flex flex-col gap-[.5rem]">
+          <span className="capitalize font-semibold productTitle">{product.title}</span>
+          <span>₹ {product.price}</span>
+          <span className="font-semibold  outline outline-2 outline-amber-50 bg-yellow-300 hover:bg-yellow-500 transition cursor-pointer px-[1rem] p-[.3rem] rounded-md hover:text-slate-50 text-slate-900 text-[1rem] max-w-[6rem]" 
+          onClick = {
+            ()=>{
+              dispatch({type:'removeProduct', productId: product.id})
+            }
+          }
+          >Remove</span>
         </div>
       </div>
 
-      <div className="flex flex-col gap-[.5rem] font-semibold text-[2rem] items-center">
+      <div className="productQuantityUpdateController flex flex-col gap-[.5rem] font-semibold text-[2rem] items-center">
         <i onClick={
           ()=>{
-            // dispatch({type:'increment'})
+            dispatch({type:'increaseQuantity', productId: product.id})
+            
           }
         } className="fa-sharp fa-solid fa-chevron-up font-semibold  outline outline-2 outline-amber-50 bg-yellow-300 hover:bg-yellow-500 transition cursor-pointer px-[.8rem] p-[.3rem] rounded-full hover:text-slate-50 text-slate-900 text-[1.3rem] max-w-[6rem]"></i>
         <span className="">{product.quantity}</span>
         <i onClick={
           ()=>{
-            // dispatch({type:'decrement'})
+            dispatch({type:'decreaseQuantity', productId: product.id})
           }
         } className="fa-sharp fa-solid fa-chevron-down font-semibold  outline outline-2 outline-amber-50 bg-yellow-300 hover:bg-yellow-500 transition cursor-pointer px-[.8rem] p-[.3rem] rounded-full hover:text-slate-50 text-slate-900 text-[1.3rem] max-w-[6rem]"></i>
       </div>
